0d854241ef3ddedfb1469926f2f2e7a53d268a5d
HawX <xiehaochina@vip.qq.com>
2016-06-14 16:45:29 +0800

    MainUI

--- Src/app/src/main/java/com/smartdraw/MainActivity.java
+++ Src/app/src/main/java/com/smartdraw/MainActivity.java
@@ -3,12 +3,15 @@ package com.smartdraw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
+import com.smartdraw.hawx.BaseActivity;
+
+public class MainActivity extends BaseActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        setHideStatusBar(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
