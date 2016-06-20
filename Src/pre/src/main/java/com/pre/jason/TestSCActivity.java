51e397fcee43a1e8730ab2c122ecd5c42ef43e1a
Jason Wang <563307134@qq.com>
2016-06-20 23:55:24 +0800

    add something for shapecontext

--- Src/pre/src/main/java/com/pre/jason/TestSCActivity.java
+++ Src/pre/src/main/java/com/pre/jason/TestSCActivity.java
@@ -1,17 +1,19 @@
 package com.pre.jason;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 
+import com.pre.DoodleView;
 import com.pre.R;
+
+import java.util.List;
 
 public class TestSCActivity extends AppCompatActivity
 {
-    private ImageView imageView;
+    private ImageView iv_after;
+    private DoodleView doodleView;
     private Bitmap bitmap1;
     private Bitmap bitmap2;
     private Bitmap bitmap3;
@@ -22,30 +24,41 @@ public class TestSCActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_sc);
-        imageView = (ImageView) findViewById(R.id.image);
+        iv_after = (ImageView) findViewById(R.id.iv_after);
+        doodleView = (DoodleView) findViewById(R.id.doodleView);
 
-        BitmapFactory.Options opts=new BitmapFactory.Options();
-        bitmap1= BitmapFactory.decodeResource(getResources(), R.drawable.fish1, opts);
-        bitmap2= BitmapFactory.decodeResource(getResources(), R.drawable.fish2, opts);
-        bitmap3= BitmapFactory.decodeResource(getResources(), R.drawable.fish3, opts);
-        bitmap4= BitmapFactory.decodeResource(getResources(), R.drawable.fish4, opts);
-//        Bitmap bitmap = bitmap4.copy(bitmap4.getConfig(),true);
-//        imageView.setImageBitmap(bitmap);
+//        BitmapFactory.Options opts=new BitmapFactory.Options();
+//        bitmap1= BitmapFactory.decodeResource(getResources(), R.drawable.fish1, opts);
+//        bitmap2= BitmapFactory.decodeResource(getResources(), R.drawable.fish2, opts);
+//        bitmap3= BitmapFactory.decodeResource(getResources(), R.drawable.fish3, opts);
+//        bitmap4= BitmapFactory.decodeResource(getResources(), R.drawable.fish4, opts);
+////        Bitmap bitmap = bitmap4.copy(bitmap4.getConfig(),true);
+////        imageView.setImageBitmap(bitmap);
+//
+//        float sc[][] = new float[4][60];
+//        ImageShanpeContext isc = new ImageShanpeContext();
+//        isc.getShapeContext(bitmap1,sc[0],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
+//        isc.getShapeContext(bitmap2,sc[1],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
+//        isc.getShapeContext(bitmap3,sc[2],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
+//        isc.getShapeContext(bitmap4,sc[3],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
+//
+//        float result[] = new float[3];
+//        for(int i=1;i<4;i++)
+//        {
+//            result[i-1]=isc.getSimilarity(sc[0],sc[i],60);
+//        }
+//        Log.d("result", "0-1 " + result[0]);
+//        Log.d("result","0-2 "+result[1]);
+//        Log.d("result","0-3 "+result[2]);
+    }
 
-        float sc[][] = new float[4][60];
+    public void getSCs(List<Bitmap> bitmaps, float shapeContext[][])
+    {
         ImageShanpeContext isc = new ImageShanpeContext();
-        isc.getShapeContext(bitmap1,sc[0],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
-        isc.getShapeContext(bitmap2,sc[1],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
-        isc.getShapeContext(bitmap3,sc[2],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
-        isc.getShapeContext(bitmap4,sc[3],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
-
-        float result[] = new float[3];
-        for(int i=1;i<4;i++)
+        int i;
+        for(i=0;i<bitmaps.size();++i)
         {
-            result[i-1]=isc.getSimilarity(sc[0],sc[i],60);
+            isc.getShapeContext(bitmaps.get(i),shapeContext[i],125,300,ImageShanpeContext.CENTER_OF_BITMAP);
         }
-        Log.d("result", "0-1 " + result[0]);
-        Log.d("result","0-2 "+result[1]);
-        Log.d("result","0-3 "+result[2]);
     }
 }
